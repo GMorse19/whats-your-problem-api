4832acf6630129ab3723e818df31ed1a609de3c5
GMorse19 <malorse@comcast.net>
2020-04-19 11:27:10 -0400

    Working on adding devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :admins
   resources :problems, except: %i[new edit]
   # RESTful routes
   resources :examples, except: %i[new edit]
