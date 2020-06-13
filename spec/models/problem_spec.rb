e9910c30b606fce667bc4c33c9be73a584d31cda
GMorse19 <malorse@comcast.net>
2020-06-13 09:47:26 -0400

    Cleanup test

--- spec/models/problem_spec.rb
+++ spec/models/problem_spec.rb
@@ -3,16 +3,13 @@ require 'rails_helper'
 RSpec.describe Problem, type: :model do
   describe 'creation' do
     it 'can be created' do
-      problem = Problem.create(
-                                name: 'testname',
-                                content: 'testcontent',
-                                hint: 'testhint',
-                                solution: 'testsolution',
-                                rating: 0,
-                                answer: 'testanswer',
-                                category: 'testcategory'
-                                  )
-
+      problem = Problem.create(name: 'testname',
+                               content: 'testcontent',
+                               hint: 'testhint',
+                               solution: 'testsolution',
+                               rating: 0,
+                               answer: 'testanswer',
+                               category: 'testcategory')
       expect(problem).to be_valid
     end
   end
